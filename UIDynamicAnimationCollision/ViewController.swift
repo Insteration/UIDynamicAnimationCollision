ece15841c39c9ae46896b3eaaa73b8d499a21bdb
Art Karma <inste@icloud.com>
2019-04-24 19:50:39 +0300

    First commit

--- UIDynamicAnimationCollision/ViewController.swift
+++ UIDynamicAnimationCollision/ViewController.swift
@@ -9,10 +9,21 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var square = UIView()
+    var animator: UIDynamicAnimator!
+
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        square.frame = CGRect(x: 50, y: 50, width: 50, height: 50)
+        square.backgroundColor = .green
+        self.view.addSubview(square)
+        
+        animator = UIDynamicAnimator(referenceView: self.view)
+        let gravity = UIGravityBehavior(items: [square])
+//        gravity.gravityDirection = CGVector(dx: -0.1, dy: 0.5)
+        animator.addBehavior(gravity)
     }
 
 
